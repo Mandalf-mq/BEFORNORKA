98baaca2bdce68948dfb098654fc75ab52954ea0
Mandalf-mq <de.sousa.barros.alfredo@gmail.com>
2025-08-21 18:23:49 -0400

    Updated TrainingCalendar.tsx

--- src/components/Training/TrainingCalendar.tsx
+++ src/components/Training/TrainingCalendar.tsx
@@ -24,7 +24,7 @@ export const TrainingCalendar: React.FC = () => {
     start_time: '',
     end_time: '',
     location: '',
-    category: ['senior'],
+    category: [''],
     coach: '',
     max_participants: 20
   });
