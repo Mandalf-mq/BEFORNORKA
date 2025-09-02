c9c8db256ece4c5488d0e8142aa34544d1b6ccb5
Mandalf-mq <de.sousa.barros.alfredo@gmail.com>
2025-09-02 00:44:22 -0400

    Corriger DocumentsManager avec nouveaux types

--- src/components/Documents/DocumentsManager.tsx
+++ src/components/Documents/DocumentsManager.tsx
@@ -122,7 +122,7 @@ export const DocumentsManager: React.FC = () => {
       setNewTemplate({
         name: '',
         description: '',
-        document_type: 'ffvbForm',
+        document_type: 'registration_form',
         file_name: '',
         file: null
       });
@@ -232,6 +232,13 @@ export const DocumentsManager: React.FC = () => {
 
   const getDocumentTypeLabel = (type: string) => {
     const labels: Record<string, string> = {
+      // Nouveaux noms standardisés
+      'registration_form': 'Formulaire d\'inscription',
+      'medical_certificate': 'Certificat médical',
+      'photo': 'Photo d\'identité',
+      'parental_authorization': 'Autorisation parentale',
+      'identity_copy': 'Copie pièce d\'identité',
+      // Anciens noms pour compatibilité
       'ffvbForm': 'Formulaire FFVB',
       'medicalCertificate': 'Certificat médical',
       'idPhoto': 'Photo d\'identité',
@@ -446,10 +453,11 @@ export const DocumentsManager: React.FC = () => {
                   onChange={(e) => setNewTemplate(prev => ({ ...prev, document_type: e.target.value }))}
                   className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-primary-500 focus:border-transparent"
                 >
-                  <option value="ffvbForm">Formulaire FFVB</option>
-                  <option value="medicalCertificate">Certificat médical</option>
-                  <option value="idPhoto">Photo d'identité</option>
-                  <option value="parentalConsent">Autorisation parentale</option>
+                  <option value="registration_form">Formulaire d'inscription</option>
+                  <option value="medical_certificate">Certificat médical</option>
+                  <option value="photo">Photo d'identité</option>
+                  <option value="parental_authorization">Autorisation parentale</option>
+                  <option value="identity_copy">Copie pièce d'identité</option>
                 </select>
               </div>
 
